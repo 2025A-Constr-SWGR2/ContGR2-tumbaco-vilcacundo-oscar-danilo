3ea405f3e1b85d92108f5e6d77d9b42b62d2975a
OscarTum <oscar.tumbaco01@epn.edu.ec>
2025-06-13 19:08:57 -0500

    actions-runner

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable() //Decorador
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    // console.log('log!');
+    // console.info('info!');
+    // console.debug('debug!');
+    console.error('error!');
+    console.warn('warn!');
+    return 'Hello World!!';
   }
 }
